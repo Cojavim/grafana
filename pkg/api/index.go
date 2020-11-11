1108e066800ae373ca193598e3d091651211337a
tomas.valasek <tomas.valasek@ingeteam.com>
2020-11-11 11:37:18 +0100

    index.go initial state

--- pkg/api/index.go
+++ pkg/api/index.go
@@ -133,7 +133,6 @@ func (hs *HTTPServer) setIndexViewData(c *models.ReqContext) (*dtos.IndexViewDat
 		{Text: "Divider", Divider: true, Id: "divider", HideFromTabs: true},
 		{Text: "Manage", Id: "manage-dashboards", Url: setting.AppSubUrl + "/dashboards", Icon: "gicon gicon-manage"},
 		{Text: "Playlists", Id: "playlists", Url: setting.AppSubUrl + "/playlists", Icon: "gicon gicon-playlists"},
-		{Text: "Dashboar Picker", Id: "playlists", Url: setting.AppSubUrl + "/dashpicker", Icon: "gicon gicon-playlists"},
 		{Text: "Snapshots", Id: "snapshots", Url: setting.AppSubUrl + "/dashboard/snapshots", Icon: "gicon gicon-snapshots"},
 	}
 
